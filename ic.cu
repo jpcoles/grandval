518bdd23628d0fb77485d1d6c156967e1d949fb7
Elena Gavagnin <Elena@dhcp-154-222.physik.uzh.ch>
2013-02-12 16:52:01 +0100

    Fixed energy calculation for hernquist. Added ic for hernquist.  Please enter the commit message for your changes. Lines starting

--- ic.cu
+++ ic.cu
@@ -9,6 +9,7 @@ struct iclist ics[] =
 { {ic_random,              "random",  ""},
   {ic_line,                "line",  ""},
   {ic_circular_plummer,    "circular_plummer",  ""},
+  {ic_circular_hernquist,  "circular_hernquist",""},  
   {ic_droplet,             "droplet",  ""},
   {ic_psdroplet,           "psdroplet",  ""},
   {ic_disk,                "disk",  ""},
@@ -56,7 +57,7 @@ void ic_random(struct particle *p, size_t N, pos_t R)
 }
 
 //==============================================================================
-//                                 ic_circular
+//                                 ic_circular_plummer
 //==============================================================================
 void ic_circular_plummer(struct particle *p, size_t N, pos_t R)
 {
@@ -77,7 +78,27 @@ void ic_circular_plummer(struct particle *p, size_t N, pos_t R)
         p[i].v[2] = 0;
     }
 }
-
+//=============================================================================
+//                                 ic_circular_hernquist
+//============================================================================
+void ic_circular_hernquist (struct particle *p, size_t N, pos_t R)
+ { 
+   mass_t M = 5;
+   dist_t eps = 0.05;
+   dist_t Rmin = 2*eps;   
+   size_t i;
+   for (i=0; i < N; i++)
+   {  
+       dist_t r = (pos_t)((R-Rmin) * (2*randU()-1));
+       r += Rmin * (2*(r>0)-1);
+       p[i].x[0] = 0; 
+       p[i].x[1] = r;
+       p[i].x[2] = 0;
+       p[i].v[0] = sqrt(M * abs(r) / pow(abs(r)+eps, 2.0F)) * (2*(r > 0)-1);
+       p[i].v[1] = 0;
+       p[i].v[2] = 0;
+    }  
+ }
 //============================================================================
 //                                  ic_line
 //============================================================================
